4e122b18e5313bc44f26678bea6730c94ee6ae02
Михаил <mih.kalyujniy@yandex.ru>
2022-12-08 14:22:14 +0300

    123

---
+++ zadanie1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Реализовать сортировку списка методом пузырька.
+#Должен сортироваться список чисел
+
+#получаем список чисел от пользователя
+l = list(map(lambda n: float(n) if '.' in n else int(n), (input('\nВведите список чисел через пробел: ').split())))
+
+for i in range(len(l)):
+    for j in range(len(l)-i-1):
+        if l[j] > l[j+1]:
+            l[j], l[j+1] = l[j+1], l[j]
+
+print(l)
