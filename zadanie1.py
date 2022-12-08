eb620d96a133bf71f621ab04287236ec6653ca96
Михаил <mih.kalyujniy@yandex.ru>
2022-12-08 15:45:03 +0300

    123

--- zadanie1.py
+++ zadanie1.py
@@ -3,11 +3,11 @@
 #Должен сортироваться список чисел
 
 #получаем список чисел от пользователя
-l = list(map(lambda n: float(n) if '.' in n else int(n), (input('\nВведите список чисел через пробел: ').split())))
+l = list(map(lambda n: float(n) if '.' in n else int(n), (input('\nВведите числа списка для сортировки через пробел: ').split())))
 
 for i in range(len(l)):
     for j in range(len(l)-i-1):
         if l[j] > l[j+1]:
             l[j], l[j+1] = l[j+1], l[j]
 
-print(l)
+print(f'Отсортированный список: {l}')
